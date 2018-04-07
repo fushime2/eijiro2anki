e8b9b434e2d87c88dde16bffbbf6e36a1d01595a
fushime2 <okmt52@gmail.com>
2018-04-07 09:57:02 +0900

    C# ファイルの追加

---
+++ Convert.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Linq;
+using System.Diagnostics;
+
+namespace CsharpTest
+{
+    class Convert
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Usage: Move a csv file to this exe file.");
+                Console.ReadKey();
+                return;
+            }
+
+            string OUTNAME = args[0] + "_out.csv";
+
+            // 読み込み
+            var words = new List<string>();
+            var means = new List<string[]>();
+            using (var r = new StreamReader(args[0]))
+            {
+                string line;
+                while ((line = r.ReadLine()) != null)
+                {
+                    line = line.Replace(';', ',');
+                    string[] seps = { "///" };
+                    string[] ss = line.Split(seps, StringSplitOptions.None)
+                        .Select(s => s.TrimEnd()).ToArray();
+
+                    // 発音記号が複数あるときは前の means に追加する
+                    if (ss.Length < 2)
+                    {
+                        string[] m = means.Last();
+                        means.RemoveAt(means.Count - 1);
+                        m[0] += " " + ss[0];
+                        means.Add(m);
+                        continue;
+                    }
+                    Debug.Assert(ss.Length == 2);
+
+                    string word = ss[0];
+                    string[] seps2 = { "\\" };
+                    string[] mean = ss[1].Split(seps2, StringSplitOptions.None)
+                        .Select(s => s.Trim()).ToArray();
+                    words.Add(word);
+                    means.Add(mean);
+                }
+            }
+            Debug.Assert(words.Count == means.Count);
+
+            // 書き込み
+            using (var writer = new StreamWriter(OUTNAME, false, Encoding.UTF8))
+            {
+                foreach (var i in words.Zip(means, Tuple.Create))
+                {
+                    string word = i.Item1;
+                    string mean = String.Join("<br>", i.Item2);
+                    writer.WriteLine(word + ";" + mean);
+                }
+            }
+            Console.WriteLine(OUTNAME + " was created.");
+            Console.ReadKey();
+        }
+    }
+}
--- convert.py
+++ convert.py
@@ -1,22 +1,13 @@
-"""
-    英辞郎
-"""
-
 import csv
 
-def format_string(s):
-    """cp932 の文字が含まれてるとエラーになるので取り除く(発音記号とか)
-    けどいらなくなった
-    """
-    UNICODE = "utf-8"
-    return s.encode(UNICODE, "ignore").decode(UNICODE)
-
+# !!! ここを書き換える !!!
+INNAME = "level7.csv"
+OUTNAME = "level7_out.csv"
 
 def main():
-    fname = "level1.txt"
+    fname = INNAME
     f = open(fname, "r", encoding="utf-16")
 
-    cnt = 0
     words, means = [], []
     for line in f:
         out = [x.strip() for x in line.split("///")]
@@ -25,7 +16,6 @@ def main():
         if len(out) < 2:
             means[-1][0] += " " + out[0] # -> str
             means[-1] = [x.strip() for x in means[-1][0].split("\\") if len(x)]
-            #break
             continue
 
         assert(len(out) == 2)
@@ -33,21 +23,11 @@ def main():
         mean = [x.strip() for x in mean.split("\\") if len(x)]
         words.append(word)
         means.append(mean)
-        
-        cnt += 1
-        #if cnt > 5: break
 
     f.close()
-
     assert(len(words) == len(means))
-    print("")
 
-    #for word, mean in zip(words, means):
-    #    #if word != "live": continue
-    #    print(word)
-    #    print(mean)
-
-    outname = "level1.csv"
+    outname = OUTNAME
     with open(outname, "w", encoding="utf-8") as f:
         writer = csv.writer(f, delimiter=";", lineterminator="\n")
         for word, mean in zip(words, means):
@@ -55,7 +35,6 @@ def main():
             writer.writerow([word, "<br>".join(mean)])
 
     print("done")
-
 
 
 if __name__ == "__main__":
